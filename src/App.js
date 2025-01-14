c2a82f8cccdf4755074951225345b33e4628e0c8
recep-demir <demir.rp@gmail.com>
2025-01-14 17:04:18 +0100

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-import './App.css';
+import './App.scss';
+import React from 'react'
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <div className="App">
+      <AppRouter/>
       
     </div>
   );
