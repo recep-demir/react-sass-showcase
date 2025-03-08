4cbbd748e99acaf3b56bec60d9d7de20a05ce9ce
recep-demir <demir.rp@gmail.com>
2025-03-08 23:03:49 +0100

    style: format code with prettier

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,54 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "./Navbar.scss"
-
+import React from "react";
+import "./Navbar.scss";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <NavLink to ="/"  className="nav__link">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav__item">
-          <NavLink to ="/about"  className="nav__link">
-              About Me
-            </NavLink>
-          </li>
-          <li className="nav__item">
-          <NavLink to ="/projects"  className="nav__link">
-              My Projects
-            </NavLink>
-          </li>
-          <li className="nav__item">
-          <NavLink to ="/contact"  className="nav__link">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <NavLink
+            to="/"
+            className="nav__link"
+            style={({ isActive }) => {
+              return { color: isActive ? "#ff652f" : "inherit" };
+            }}>
+            {" "}
+            Home{" "}
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/about"
+            className="nav__link"
+            style={({ isActive }) => {
+              return { color: isActive ? "#ff652f" : "inherit" };
+            }}>
+            About Me
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/projects"
+            className="nav__link"
+            style={({ isActive }) => {
+              return { color: isActive ? "#ff652f" : "inherit" };
+            }}>
+            My Projects
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink
+            to="/contacts"
+            className="nav__link"
+            style={({ isActive }) => {
+              return { color: isActive ? "#ff652f" : "inherit" };
+            }}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
